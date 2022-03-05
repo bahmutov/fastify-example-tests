2a5369b6f7c64c263298e348a833c7e87a835c2e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-05 13:39:45 -0500

    spec01

---
+++ README.md
@@ -0,0 +1,5 @@
+# fastify-example-tests
+
+> Cypress tests to fill to check [fastify-example](https://github.com/bahmutov/fastify-example) application
+
+The public repository has test placeholders to be filled by the actual Cypress commands.
---
+++ cypress/integration/spec01.js
@@ -0,0 +1,13 @@
+// make sure the IntelliSense works in this JavaScript spec file
+it('shows some fruit', () => {
+  // define baseUrl in cypress.json
+  // set the convenient viewport in cypress.json
+  // visit the page
+  // get the fruit element by ID
+  // and confirm it does not have the loading text
+  // and confirm it has the fruit name which is a word
+  // that starts with a capital letter
+  // followed by all lowercase letters
+  // alternative: use cy.contains with a regular expression
+  // see https://on.cypress.io/contains
+})
