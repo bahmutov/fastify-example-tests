20e76a30889568ce715a48e62c0ec8c8c51d3670
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-05 14:36:54 -0500

    spec04

--- cypress/integration/spec04.js
+++ cypress/integration/spec04.js
@@ -1,1 +1,10 @@
 /// <reference types="cypress" />
+
+it('clearly shows the loading element', () => {
+  // stub the network call the application makes
+  // and delay returning the fruit by 2 seconds
+  // visit the page
+  // check if the loading element is visible
+  // and then does not exist
+  // confirm the displayed fruit
+})
