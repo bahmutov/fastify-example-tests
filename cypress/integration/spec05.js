233e1ee9a448aece1090d2c8324bc57106d2cb01
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-06 12:43:19 -0500

    spec5

--- cypress/integration/spec05.js
+++ cypress/integration/spec05.js
@@ -1,1 +1,10 @@
 /// <reference types="cypress" />
+
+it('shows a different fruit after reloading the page', () => {
+  // visit the site using https://on.cypress.io/visit
+  // grab the fruit name from the page
+  // (make sure it is not "loading...")
+  // reload the page using https://on.cypress.io/reload
+  // grab the fruit name from the page again
+  // confirm the fruit name is different
+})
