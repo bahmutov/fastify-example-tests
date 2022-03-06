04738ff7b409729a9fb6eb657de6e5215fa4ca72
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-06 14:27:18 -0500

    spec10

--- cypress/integration/spec09.js
+++ cypress/integration/spec09.js
@@ -1,1 +1,10 @@
 /// <reference types="cypress" />
+
+it('returns different fruits', () => {
+  // stub the /fruit endpoint to return "apple" on the first visit
+  // stub the /fruit endpoint to return "grapes" on the second visit
+  // visit the site
+  // confirm it shows "apple"
+  // reload the site
+  // confirm it shows "grapes"
+})
--- cypress/integration/spec10.js
+++ cypress/integration/spec10.js
@@ -1,1 +1,13 @@
 /// <reference types="cypress" />
+
+it('shows the loading element with Promise.delay', () => {
+  // set up an intercept to spy on the GET /fruit endpoint
+  // but delay the request by 2 seconds using
+  // Cypress.Promise.delay(200) and the response function
+  // hint: see https://on.cypress.io/intercept and
+  // the response callback function
+  // visit the site
+  // make sure the loading element is visible
+  // from the request spy, get the response body
+  // and confirm the fruit returned by the server is visible
+})
