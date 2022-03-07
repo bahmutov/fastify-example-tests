ac07c7327a5e8984172c72957d4910797115ae0d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-07 09:02:00 -0500

    spec 14

---
+++ cypress/integration/spec13.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+it('fetches 10 fruits from the server', () => {
+  // spy on the GET /fruit network call
+  // and store each received fruit in a list
+  // freeze the clock before visiting the page
+  // confirm there is one fruit in the list after loading the page
+  // advance the clock by 9 minutes
+  // and confirm the list of fruits has 10 items
+  // and it includes "Oranges"
+})
---
+++ cypress/integration/spec14.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+it('reloads the page until it shows Bananas', () => {
+  // visit the page
+  // if it shows the fruit "Bananas", stop
+  // else
+  //   wait for 1 second for clarity
+  //   reload the page
+  //   check again
+  // Tip: use recursion
+})
