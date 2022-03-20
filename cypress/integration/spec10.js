5d25901fe73fc4e6c0bb6f27035aa511de32a723
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-20 12:56:29 -0400

    fix the delay comment

--- cypress/integration/spec10.js
+++ cypress/integration/spec10.js
@@ -3,7 +3,7 @@
 it('shows the loading element with Promise.delay', () => {
   // set up an intercept to spy on the GET /fruit endpoint
   // but delay the request by 2 seconds using
-  // Cypress.Promise.delay(200) and the response function
+  // Cypress.Promise.delay(2000) and the response function
   // hint: see https://on.cypress.io/intercept and
   // the response callback function
   // visit the site
