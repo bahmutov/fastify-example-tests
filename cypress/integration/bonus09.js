798736f326e229d9e8124972520dc184105db895
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-04-20 08:54:46 -0400

    add bonus 9

---
+++ cypress/integration/bonus09.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+it('requests an image and gets its dimensions', () => {
+  // request the image ourselves using cy.request command
+  // with the "encoding: base64" option
+  //
+  // from the response get the body and the headers fields
+  // from the headers object, grab the "content-type" header
+  // which gives us the correct mime type, like "image/png"
+  //
+  // form the image data URL with the mime type and the base64 encoded image
+  // following the template `data:${mime};base64,${body}`
+  //
+  // to parse the data URL and have an image element
+  // use document.createElement and set the "src" attribute
+  // from the image element, get the naturalHeight property
+  // and confirm its value is 914 pixels
+})
