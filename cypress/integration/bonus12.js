8cb4528a71608e70c7ae0fb97e0b0154832c26dc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-04-28 07:31:14 -0400

    add bonus12

---
+++ cypress/integration/bonus12.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+it('saves the intercepted network response', () => {
+  // intercept the GET /fruit request
+  // https://on.cypress.io/intercept
+  // and print the server response using "cy.log" command
+  // https://on.cypress.io/log
+  //
+  // visit the site using cy.visit("/")
+  //
+  // NOTE: you cannot use "cy" commands inside the
+  // intercept route handler. Instead, save the response
+  // in a local closure variable "response"
+  // and wait for the network call to finish
+  // then print the saved response using the "cy.log"
+  // command and save it into a file "response.json"
+  // https://on.cypress.io/writefile
+  let response
+  cy.intercept('GET', '/fruit', (req) => {
+    req.continue((res) => {
+      response = res.body
+    })
+  }).as('fruit')
+  cy.visit('/')
+  cy.wait('@fruit').then(() => {
+    expect(response, 'got the response').to.be.an('object')
+    cy.log(response)
+    cy.writeFile('response.json', response)
+  })
+})
