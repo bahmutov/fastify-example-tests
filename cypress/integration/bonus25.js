b734e3d703c03859b085391824a5d79400c7e3cf
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-06-10 09:09:53 -0400

    add bonus25

---
+++ cypress/integration/bonus25.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+it('mocks the response by inspecting the request body', () => {
+  // visit the page /calculator.html
+  // https://on.cypress.io/visit
+  //
+  // intercept the POST requests to the /calculate endpoint
+  // https://on.cypress.io/intercept
+  // from the reply, grab the body's "operation" property
+  // If it is equal to "+" then assign the request alias "add"
+  // and reply with the sub of the request body's "a" and "b"
+  //
+  // give the intercept alias "calculate"
+  // https://on.cypress.io/as
+  //
+  // type "2" into the input #num1
+  // type "3" into the input #num2
+  // https://on.cypress.io/get
+  // https://on.cypress.io/type
+  //
+  // click the "add" button
+  // https://on.cypress.io/click
+  //
+  // confusingly, the intercept has the "wait" alias
+  // but NOT the "calculate" alias, even though
+  // the Cypress Command Log shows the "calculate" alias
+  // wait for the "@add" intercept
+  // https://on.cypress.io/wait
+  //
+  // confirm the answer shown is correct
+  // https://on.cypress.io/contains
+})
