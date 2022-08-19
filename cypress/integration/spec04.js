2543ed36686657e6af6a32f65a976d031254f4b5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-08-19 13:55:27 -0400

    add spec4 bonus

--- cypress/integration/spec04.js
+++ cypress/integration/spec04.js
@@ -16,3 +16,22 @@ it('clearly shows the loading element', () => {
   // confirm the displayed fruit
   // https://on.cypress.io/contains
 })
+
+// bonus - instead of stubbing the request, just delay it
+it('slows down the /fruit request without stubbing it', () => {
+  // instead of stubbing the request GET /fruit
+  // return a Bluebird Promise that delays the action
+  // by 2 seconds. After that Cypress will continue
+  // with the network request going to the server
+  // https://on.cypress.io/intercept
+  // https://on.cypress.io/promise delay method
+  // Hint: see the lesson spec10
+  //
+  // visit the page "/""
+  //
+  // check if the loading element is visible
+  // and has the text "loading..."
+  //
+  // and then the fruit element should
+  // not have the text "loading"
+})
