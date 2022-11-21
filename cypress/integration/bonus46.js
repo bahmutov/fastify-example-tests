e46c8b7cda800ada7f1593d43e5eb238aa43af55
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-21 09:21:44 -0500

    add bonus46

---
+++ cypress/integration/bonus46.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+// finish implementing and refactoring this test
+it.skip('successfully completes every network request', () => {
+  // intercept all requests and let them continue
+  // going to the server. When the server replies,
+  // check the response status code. It should be below 400.
+  // https://on.cypress.io/intercept
+  cy.intercept('*', (req) => {
+    req.continue((res) => {
+      expect(res.statusCode, 'status code').to.be.oneOf([
+        200, 304,
+      ])
+    })
+  })
+  // visit the page "/bundles" and see if the test fails
+  // https://on.cypress.io/visit
+  cy.visit('/bundles')
+})
