86316362621c521f5918efe7d39346bc7bd3b970
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-02 15:05:58 -0500

    more comments

--- cypress/integration/spec14.js
+++ cypress/integration/spec14.js
@@ -1,11 +1,21 @@
 /// <reference types="cypress" />
 
 it('reloads the page until it shows Bananas', () => {
-  // visit the page
+  // visit the page "/"
+  // https://on.cypress.io/visit
   // if it shows the fruit "Bananas", stop
+  //
   // else
   //   wait for 1 second for clarity
   //   reload the page
   //   check again
-  // Tip: use recursion
+  //
+  // Tip: use the commands like
+  // https://on.cypress.io/get
+  // https://on.cypress.io/invoke
+  // https://on.cypress.io/then
+  // https://on.cypress.io/wait
+  // https://on.cypress.io/reload
+  //
+  // Tip 2: use recursion
 })
