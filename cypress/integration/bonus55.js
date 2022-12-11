459abe594738d00e0f6268cf11d5f1b9f6964506
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-11 17:38:29 -0500

    add bonus 55

---
+++ cypress/integration/bonus55.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+it('makes 3 API calls to fetch the fruit', () => {
+  // spy on the the calls to the "GET /fruit" calls
+  // and give the intercept alias "fruit"
+  // https://on.cypress.io/intercept
+  // https://on.cypress.io/as
+  //
+  // visit the home page
+  // https://on.cypress.io/visit
+  //
+  // wait for the fruit to load
+  // Tip: confirm the text "loading" is gone
+  // https://on.cypress.io/contains
+  //
+  // reload the page and confirm it loads a fruit
+  // https://on.cypress.io/reload
+  //
+  // again reload the page and confirm the fruit loads
+  //
+  // get all fruit network calls and confirm
+  // there were 3 network calls made by the application
+  // https://on.cypress.io/get
+})
