d229260e55d1c81e6ff36e3d79d1e69217450f39
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-12 17:08:44 -0500

    add bonus56

---
+++ cypress/integration/bonus56.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+it('validates the referer header URL', () => {
+  // spy on the network call the application makes
+  // tip: use https://on.cypress.io/intercept
+  // and give the intercept alias "fruit"
+  // https://on.cypress.io/as
+  //
+  // visit the page "/"
+  // https://on.cypress.io/visit
+  //
+  // wait for the network call to finish
+  // https://on.cypress.io/wait
+  //
+  // grab the "referer" request header
+  // https://on.cypress.io/its
+  //
+  // Tip: confirm it is a string to print it
+  //
+  // construct the URL instance from the string
+  // https://developer.mozilla.org/en-US/docs/Web/API/URL
+  //
+  // from the URL instance, we can grab multiple properties
+  // that we can check. For example the "host"
+  // is our base Url without the protocol
+})
