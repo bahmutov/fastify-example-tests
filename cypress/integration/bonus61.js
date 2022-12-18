3b7091dce50ea122038fa67676ea7b6b6b2eafe3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-18 14:05:00 -0500

    add bonus61

---
+++ cypress/integration/bonus61.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+it('modifies the outgoing request', () => {
+  // visit the page "calculator.html"
+  // https://on.cypress.io/visit
+  //
+  // intercept the "POST /calculate" calls
+  // look at the request body and multiply
+  // the "a" and "b" arguments by 100
+  //
+  // type "2" into the input #num1
+  // type "3" into the input #num2
+  // https://on.cypress.io/get
+  // https://on.cypress.io/type
+  //
+  // click the "add" button
+  // https://on.cypress.io/click
+  //
+  // confirm the answer shown is 500
+})
