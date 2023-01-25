f209fa4d2c36631cbb01bcf1d3a144a65377ea89
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-01-25 12:54:21 -0500

    add bonus76

---
+++ cypress/integration/bonus76.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+// @ts-check
+
+// before each test generate a random test ID string
+// and attach it to every GET and POST network call
+// made by the application. Use the request header name "x-test-id"
+// https://on.cypress.io/intercept
+beforeEach(() => {
+  const randomId = 'test-id-' + Cypress._.random(1e6)
+  cy.intercept(
+    {
+      method: /GET|POST/,
+    },
+    (req) => {
+      req.headers['x-test-id'] = randomId
+    },
+  )
+})
+
+it('sends common X request id header on all network calls', () => {
+  // observe the terminal output from the fastify-example server
+  // you should see the same "x-test-id" request header
+  // on multiple requests
+  cy.visit('/calculator.html')
+  cy.get('#num1').type('10')
+  cy.get('#num2').type('5')
+  cy.contains('+').click()
+  cy.contains('#answer', '15')
+})
